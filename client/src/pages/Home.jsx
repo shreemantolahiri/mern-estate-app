a6952d23a67f77d2843071aa5a99152f56b6a536
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-04-18 02:34:57 +0530

    fixed some frontend bugs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,10 +55,10 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Beluga Estate is the best place to find your next perfect place to
           live.
           <br />
-          We have a wide range of properties for you to choose from.
+          We have a wide range of properties for you to choose from. Feel free to go through all the listings and find your next perfect place.
         </div>
         <Link
           to={'/search'}
